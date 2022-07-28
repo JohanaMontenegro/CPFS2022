8d7b8d6e51ffec375ef43606ec6acf7ca88dbc7e
JohanaMontenegro <yoh.teencontre@gmail.com>
2022-07-28 13:33:06 -0300

    clase-1

---
+++ clase_1/alturaPersona.js
@@ -0,0 +1,3 @@
+let readlineSync=require('readline-Sync');
+let alturaPersona=readlineSync.question('indique la altura: ');
+console.log(alturaPersona);
---
+++ clase_1/baseXaltura.js
@@ -0,0 +1,5 @@
+let readline= require('readline-sync');
+let altura=readline.question('ingrese altura: ');
+let base=readline.question('ingrese base: ');
+let resultado=base*altura;
+console.log(resultado);
