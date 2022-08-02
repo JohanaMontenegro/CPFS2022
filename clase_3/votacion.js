25eebc28129d4250d1dbd39dd86326f264ae0ea5
JohanaMontenegro <yoh.teencontre@gmail.com>
2022-08-02 20:09:03 -0300

    modificaciones clase-3

--- clase_3/votacion.js
+++ clase_3/votacion.js
@@ -3,11 +3,21 @@ exports.__esModule = true;
 var fs = require("fs");
 var persona = fs.readFileSync("votantes.txt", "utf8");
 var lugar = fs.readFileSync("colegios.txt", "utf8");
-var votante = persona.split(" ");
+var votante = persona.split(",");
 console.log(votante);
-var ubicacion = lugar.split(" ");
+var ubicacion = lugar.split(",");
 console.log(ubicacion);
-function buscarVotante(votando) {
-    if (votante.indexOf(votando) != -1)
-        console.log('el votante${votando}$ corresponde votar en el lugar de ubicacion con indice ${votante.indexOf(votando)}$');
+function dondeVotar(nombre) {
+    var i = votante.indexOf(nombre);
+    if (i == -1) {
+        console.log("la persona " + nombre + " no esta registrada en el padrón");
+    }
+    else if (i == 0) {
+        console.log("la persona " + nombre + " le corresponde votar en el colegio " + ubicacion[0]);
+    }
+    else if (i == 1) {
+        console.log("la persona " + nombre + " le corresponde votar en el colegio " + ubicacion[1]);
+    }
 }
+dondeVotar("MariaPaz");
+dondeVotar("Franco");
--- clase_3/votacion.ts
+++ clase_3/votacion.ts
@@ -2,11 +2,33 @@ import *as fs from "fs";
 
 let persona: string = fs.readFileSync("votantes.txt", "utf8");
 let lugar: string = fs.readFileSync("colegios.txt", "utf8");
-let votante: string[] = persona.split(" ");
+let votante: string[] = persona.split(",");
 console.log(votante);
-let ubicacion: string[] = lugar.split(" ");
+let ubicacion: string[] = lugar.split(",");
 console.log(ubicacion);
 
-function dondeVotar(){
-    
+function dondeVotar(nombre: string): void {
+    let i = votante.indexOf(nombre);
+    if (i == -1) {
+        console.log("La persona " + nombre + " no esta registrada en el padrón");
+    }
+    else if (i == 0) {
+        console.log("La persona " + nombre + " le corresponde votar en el colegio " + ubicacion[0]);
+    }
+    else if (i == 1) {
+        console.log("La persona " + nombre + " le corresponde votar en el colegio " + ubicacion[1]);
+    }
+
+    else if (i == 2) {
+        console.log(" La ersona " + nombre + " le corresponde votar en el colegio " + ubicacion[2]);
+    }
+
+    else if (i == 3) {
+        console.log("La persona " + nombre + " le corresponde votar en el colegio " + ubicacion[3]);
+    }
+
 }
+dondeVotar("MariaPaz");
+dondeVotar("Franco");
+dondeVotar("Pilar");
+dondeVotar("Marcelo");
